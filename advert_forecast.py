fa2377153e9d25ef47265a25bc0275051c172d0c
PiotrSobczak <piosobc@gmail.com>
2017-10-02 04:52:20 +0200

    First successful training

--- advert_forecast.py
+++ advert_forecast.py
@@ -4,6 +4,23 @@ import numpy as np
 from matplotlib import pyplot as plt
 import tensorflow as tf
 
+class DataWrapper:
+    def __init__(self,x,y,data_size,batch_size):
+        self.data = x
+        self.labels = y
+        self.data_size = data_size
+        self.batch_size = batch_size
+        self.next_batch_ind = 0
+
+    def get_next_batch(self):
+        batch_x = self.data[self.next_batch_ind:self.next_batch_ind+batch_size]
+        batch_y = self.labels[self.next_batch_ind:self.next_batch_ind+batch_size]
+        self.next_batch_ind += self.batch_size
+        if self.next_batch_ind + self.batch_size > self.data_size:
+            self.next_batch_ind = 0
+        #print(batch_x,batch_y)
+        return batch_x,batch_y
+
 def plot_corr_mat(df):
     corr = df.corr()
     # Generate a mask for the upper triangle
@@ -81,8 +98,87 @@ if __name__=='__main__':
 
     #Normalizing inputs
     X = (X - X.min() - (X.max() - X.min()) / 2) / ((X.max() - X.min()) / 2)
-    
-    print("X:",X)
+
+    #Splitting into train,val,test sets
+    X_train = np.array(X[:530])
+    Y_train = np.array(Y[:530])
+    X_val = np.array(X[530:630])
+    X_val = np.array(Y[530:630])
+    X_test = np.array(X[630:])
+    Y_test = np.array(Y[630:])
+
+    train_data = DataWrapper(X_train,Y_train,530,10)
+    val_data = DataWrapper(X_val, X_val, 100, 10)
+    test_data = DataWrapper(X_test, Y_test, 100, 10)
+
+    # Defining input/output placeholders
+    X = tf.placeholder(dtype=tf.float32, shape=[None, 11])
+    Y = tf.placeholder(dtype=tf.float32, shape=[None])
+    keep_prob = tf.placeholder(tf.float32)
+
+    stddev = 0.01
+    learning_rate = 0.001
+    epochs = 100
+    batch_size = 10
+    steps_in_epoch = 530//10
+    num_eval_imgs = 500
+    # Defining weights and biases
+    W = {
+        "fc": tf.Variable(tf.random_normal([11, 30], stddev=stddev)),
+        "out": tf.Variable(tf.random_normal([30, 1], stddev=stddev)),
+    }
+
+    B = {
+        "fc": tf.Variable(tf.random_normal([30], stddev=stddev)),
+        "out": tf.Variable(tf.random_normal([1], stddev=stddev)),
+    }
+
+    # Defining our model
+    #fc = tf.reshape(X, [-1, W['fc'].get_shape().as_list()[0]])
+    fc = tf.add(tf.matmul(X, W['fc']), B['fc'])
+    fc = tf.nn.relu(fc)
+
+    # Apply Dropout
+    #fc = tf.nn.dropout(fc, keep_prob)
+
+    # Output
+    output = tf.add(tf.matmul(fc, W['out']), B['out'])
+
+    # Define loss and optimizer
+    loss_op = tf.reduce_mean(tf.abs(output-Y))
+
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    train_op = optimizer.minimize(loss_op)
+
+    #correct_pred = tf.equal(tf.argmax(output, 1), tf.argmax(Y, 1))
+    #total_correct = tf.reduce_sum(tf.cast(correct_pred, tf.int32))
+    #accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+    # Initialize the variables (i.e. assign their default value)
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        # Run the initializer
+        sess.run(init)
+        best_val_acc = 0
+        val_acc_his = np.zeros(epochs)
+        train_acc_his = np.zeros(epochs)
+        for epoch in range(epochs):
+            train_loss = sess.run(loss_op, feed_dict={X: train_data.data[:num_eval_imgs], Y: train_data.labels[:num_eval_imgs], keep_prob: 1.0})
+            train_acc_his[epoch] = train_loss
+            print("Accuracy after", str(epoch), "epochs: Train set:", train_loss)
+            for step in range(steps_in_epoch):
+                batch_x, batch_y = train_data.get_next_batch()
+                #print(batch_x,batch_y)
+                # Run optimization op (backprop)
+                _, pred, loss_ = sess.run([train_op, output, loss_op],
+                                          feed_dict={X: batch_x, Y: batch_y, keep_prob: 1.0})
+                #print(pred,loss_)
+
+        plt.plot(train_acc_his)
+        plt.show()
+        print("Optimization Finished!")
+
 
 
 
